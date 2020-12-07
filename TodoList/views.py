dfc5a3024d6fbccf2cfa476e24a8c246eb7db734
Alireza <Alirezakamyab19@gmail.com>
2020-12-07 13:50:02 +0330

    Not complete yet

--- TodoList/views.py
+++ TodoList/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import List
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    all_items = List.objects.all()
+    context = {'items':all_items}
+    return render(request, 'home.html', context)
 
 def about(request):
     return render(request, 'about.html', {})
